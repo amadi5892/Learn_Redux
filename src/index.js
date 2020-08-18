3ec02a7f8c011d5256d841050ad719e599af9fe2
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-17 20:15:02 -0400

    counter redux example complete

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import allReducers from './reducers';
+import {Provider} from 'react-redux';
 
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,9 +1,11 @@
 const counterReducer = (state = 0, action) => {
     switch(action.type){
         case 'INCREMENT':
-            return state + 1;
-        case 'Decrement':
+            return state + action.payload;
+        case 'DECREMENT':
                 return state - 1;
+        default:
+            return state;
     }
 };
 
